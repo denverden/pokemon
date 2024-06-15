9e9d574aa67a78be15f2bb0b2b1df0e7ae38fc2d
DenverDen <denverden@mail.ru>
2024-06-16 01:08:29 +0300

    feat: added mock data and listview

--- app/src/main/java/com/example/pokemon/MainActivity.kt
+++ app/src/main/java/com/example/pokemon/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.pokemon
 
-import android.os.Binder
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pokemon.databinding.ActivityMainBinding
+import com.example.test2.Pokemon
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,5 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val items = listOf(
+            Pokemon(4, "Charmander", 6, 85, listOf("Fire"), "image"),
+            Pokemon(25, "Pikachu", 4, 60, listOf("Electric"), "image"),
+            Pokemon(9, "Blastoise", 16, 855, listOf("Water"), "image"),
+        )
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items.map { it.name })
+
+        binding.listView.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/example/pokemon/model/Pokemon.kt
@@ -0,0 +1,11 @@
+package com.example.test2
+
+class Pokemon(
+    val id: Int,
+    val name: String,
+    val height: Int,
+    val weight: Int,
+    val type: List<String>,
+    val image: String
+) {
+}
