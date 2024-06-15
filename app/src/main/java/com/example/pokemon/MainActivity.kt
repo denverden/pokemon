d456d7ed5ef0636b127614224d7ea0665236e8d3
DenverDen <denverden@mail.ru>
2024-06-15 20:03:57 +0300

    feat: added binding

--- app/src/main/java/com/example/pokemon/MainActivity.kt
+++ app/src/main/java/com/example/pokemon/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.pokemon
 
+import android.os.Binder
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.pokemon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
