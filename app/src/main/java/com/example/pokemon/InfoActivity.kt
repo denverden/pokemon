e629e50f5123b8eb456bca79a9dea48a547ba7e0
DenverDen <denverden@mail.ru>
2024-06-19 10:58:28 +0300

    feat: added info layout activity

---
+++ app/src/main/java/com/example/pokemon/InfoActivity.kt
@@ -0,0 +1,18 @@
+package com.example.pokemon
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pokemon.databinding.ActivityInfoBinding
+
+class InfoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInfoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
