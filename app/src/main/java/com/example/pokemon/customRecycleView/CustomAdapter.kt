57e078aa3c08cd399befa6f666cc864fe2c5e117
DenverDen <denverden@mail.ru>
2024-06-24 00:37:31 +0300

    feat: added diffutil

--- app/src/main/java/com/example/pokemon/customRecycleView/CustomAdapter.kt
+++ app/src/main/java/com/example/pokemon/customRecycleView/CustomAdapter.kt
@@ -2,11 +2,21 @@ package com.example.pokemon.customRecycleView
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pokemon.customDiffUtil.CustomDiffUtil
 import com.example.pokemon.databinding.CustomCardViewBinding
 import com.example.pokemon.model.Pokemon
 
 class CustomAdapter(private val items: List<Pokemon>, private val itemOnClick: (Int) -> Unit) : RecyclerView.Adapter<CustomViewHolder>() {
+    private var oldItems: List<Pokemon> = listOf()
+
+    fun submitItems(newItems: List<Pokemon>) {
+        val diffCallback = CustomDiffUtil(oldItems, newItems)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
+        oldItems = newItems
+        diffResult.dispatchUpdatesTo(this)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         val binding = CustomCardViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
